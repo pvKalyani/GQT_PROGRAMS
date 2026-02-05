b45ee622b72456a4f47611266fe9dfa5243dc10a
pvKalyani <pvkalyanikalyani5@gmail.com>
2026-02-05 08:41:03 +0530

    Duplicate 'hello' print statements in Launch1.java

--- src/demo/Launch1.java
+++ src/demo/Launch1.java
@@ -3,6 +3,9 @@ package demo;
 public class Launch1 {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
 	}
 
 }
